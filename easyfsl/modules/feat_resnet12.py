8bb8f7bc3793097fab09b9062dbf3c205b9bfe47
Adnan Ferdous Ashrafi <nazib91@yahoo.com>
2024-01-28 15:18:18 +0600

    Update feat_resnet12.py

--- easyfsl/modules/feat_resnet12.py
+++ easyfsl/modules/feat_resnet12.py
@@ -280,7 +280,6 @@ class FEATResNet12(nn.Module):
         """
         Iterate over the blocks and apply them sequentially.
         """
-        x = self.stn(x)
         x = self.layer3(self.layer2(self.layer1(x)))
         return x.mean((-2, -1))
 
